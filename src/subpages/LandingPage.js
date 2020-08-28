167611876617a24eac2607c94002b128adbb8af2
Yousef Ahmad <yousef.m.ahmad@outlook.com>
2020-08-27 23:56:19 -0400

    Spacing

--- src/subpages/LandingPage.js
+++ src/subpages/LandingPage.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Navbar from "../components/Navbar"
+import Logo from "../../public/icons/Logo.png"
 import { useStaticQuery, graphql, Link } from "gatsby"
 
 function LandingPage() {
